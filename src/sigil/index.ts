0d0fde982bf48c1b96e7bd31fdacb9b7c6ab9bab
Jara Juana (Juani) Bermejo-Vega (she/her) <75273221+jbermejovega@users.noreply.github.com>
2026-07-07 18:26:24 +0200

    feat(sigil): expose palette linter module

---
+++ src/sigil/index.ts
@@ -0,0 +1,15 @@
+export {
+  UAPA_SIGIL_ACCESSIBLE_PALETTE_LINTER_ID,
+  buildSigilPaletteFlow,
+  lintAccessiblePalette,
+} from './palette-linter.js'
+
+export type {
+  SigilPaletteFlow,
+  SigilPaletteLintInput,
+  SigilPaletteLintResult,
+  SigilPalettePairing,
+  SigilPalettePdgWitness,
+  SigilPaletteTheme,
+  SigilPrimitive,
+} from './pdg-types.js'
